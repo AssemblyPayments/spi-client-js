39392ccbf0c868089a1a23632e00e04ae1ce3283
James Harris <dev@jharris.eu>
2018-10-15 23:46:35 +1100

    Update src/Service/DeviceService.js

---
+++ src/Service/DeviceService.js
@@ -0,0 +1,29 @@
+
+class DeviceIpAddressStatus
+{
+    constructor(ip, last_updated) 
+    {
+        this.Ip = ip;
+        this.Last_updated = last_updated;
+    }
+}
+
+class DeviceIpAddressService
+{
+    RetrieveService(serialNumber, apiKey = 'spi-sample-pos1')
+    {
+        var deviceIpUrl =
+            `https://device-address-api-dev.nonprod-wbc.msp.assemblypayments.com/v1/${serialNumber}/ip`;
+
+        return fetch(deviceIpUrl, {
+            method: 'GET',
+            headers: {
+                "ASM-MSP-DEVICE-ADDRESS-API-KEY": apiKey
+            }
+        })
+        .then(response => response.json())
+        .catch((response) => {
+            console.error(`Status code ${response.StatusCode} received from ${deviceIpUrl} - Exception ${response.ErrorException}`);
+        })
+    }
+}
