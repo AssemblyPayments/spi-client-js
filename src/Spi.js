7517c9150c4df287326b8cf0e2a37a5f0af6cc11
Matt Gresham <mgresham@mx51.io>
2020-03-16 09:50:41 +1100

    chore: Bump version for release

--- src/Spi.js
+++ src/Spi.js
@@ -20,7 +20,7 @@ import {TerminalStatusRequest} from './TerminalStatus';
 import {TerminalConfigurationRequest} from './TerminalConfiguration';
 import {ZipRefundRequest, ZipPurchaseRequest} from './ZipTransactions';
 
-const SPI_VERSION = '2.6.8';
+const SPI_VERSION = '2.6.9';
 
 class Spi {
 
