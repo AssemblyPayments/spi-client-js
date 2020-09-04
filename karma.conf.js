63e395ab2ec83c843be324054a8a9847b0a8b053
Raymond Ko <ray@mx51.io>
2020-09-04 15:56:16 +1000

    Implementation of Code Coverage and Unit Test Report

--- karma.conf.js
+++ karma.conf.js
@@ -85,15 +85,22 @@ module.exports = function (config) {
             stripPrefix: 'tests/fixtures/'
         },
 
+        junitReporter: {
+            outputDir: 'test/junit',
+            outputFile: 'result.xml',
+            useBrowserName: false
+         },
+         
         // test results reporter to use
         // possible values: 'dots', 'progress'
         // available reporters: https://npmjs.org/browse/keyword/karma-reporter
-        reporters: ['progress', 'coverage'],
+        reporters: ['progress', 'coverage', 'junit'],
 
         // code coverage reporter
         coverageReporter: {
             dir: 'coverage',
             reporters: [
+                { type: 'cobertura', subdir: '.', file: 'cobertura.xml' },
                 { type: 'json', subdir: '.' },
                 { type: 'lcov', subdir: '.' },
                 { type: 'text' },
